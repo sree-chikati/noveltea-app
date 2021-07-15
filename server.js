14b5cc46f9ff74862f13e5a3bd0fa4b15c184146
sree-chikati <srechi453.fusdk12@gmail.com>
2021-07-15 01:02:04 -0700

    Update DB and server

--- server.js
+++ server.js
@@ -1,15 +1,44 @@
 const express = require('express');
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+var cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+
 // App Setup
 const app = express();
 
-// Allows us to use content from Public file
-app.use(express.static('public'));
+// Auth checking
+var checkAuth = (req, res, next) => {
+    console.log("Checking authentication");
+    if (typeof req.cookies.nToken == "undefined" || req.cookies.nToken === null) {
+      console.log("No valid cookie found");
+      req.user = null;
+    } else {
+      var token = req.cookies.nToken;
+      var decodedToken = jwt.decode(token, { complete: true }) || {};
+      req.user = decodedToken.payload;
+    }
+    next();
+};
+
+// All app.use
+app.use('*/css',express.static('public/css')); // Allows us to use content from public file
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator()); // Add after body parser initialization!
+app.use(cookieParser());
+app.use(checkAuth);
 
 // Middleware
 const exphbs  = require('express-handlebars');
-
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
+
+// Requires
+require('./controllers/post.js')(app);
+require('./controllers/auth.js')(app);
+require('./data/noveltea-db');
 
 // Routes
 app.get('/', (req, res) => {
@@ -19,3 +48,5 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Noveltea listening on port localhost:3000!');
 });
+
+module.exports = app;
