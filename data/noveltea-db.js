f5d1ec84def8ee4a7797a11b7f99d62fc9014920
sree-chikati <srechi453.fusdk12@gmail.com>
2021-07-16 02:46:24 -0700

    Changed database

--- data/noveltea-db.js
+++ data/noveltea-db.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 assert = require("assert");
 
-const url = "mongodb://localhost/noveltea-db";
+const url = `mongodb+srv://dbSree:${process.env.MONGODB_PASSWORD}@startercluster.uhnfk.mongodb.net/noveltea-app?retryWrites=true&w=majority`  || 'maas-api'
 mongoose.Promise = global.Promise;
 mongoose.connect(
   url,
@@ -16,7 +16,9 @@ mongoose.connect(
     // db.close(); turn on for testing
   }
 );
-mongoose.connection.on("error", console.error.bind(console, "MongoDB connection Error:"));
+mongoose.connection.on('error', () => {
+  throw new Error(`unable to connect to database: ${url}`)
+})
 mongoose.set("debug", true);
 
-module.exports = mongoose.connection;
+module.exports = mongoose.connection
